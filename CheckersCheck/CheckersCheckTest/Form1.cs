2a19d56ad3dced7e28270432ca0d53ef09ff5001
kebabkiller <ad.nikodem@gmail.com>
2014-04-16 17:33:17 +0200

    kOMIT numer 3

--- CheckersCheck/CheckersCheckTest/Form1.cs
+++ CheckersCheck/CheckersCheckTest/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
 
 namespace CheckersCheckTest
 {
@@ -15,6 +18,46 @@ namespace CheckersCheckTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            runCamera();
+        }
+
+        private void runCamera()
+        {
+            ImageViewer viewer = new ImageViewer(); //create an image viewer
+            Capture capture = new Capture(); //create a camera captue
+            Application.Idle += new EventHandler(delegate(object sender, EventArgs e)
+            {  //run this until application closed (close button click on image viewer)
+                viewer.Image = checkCircles(capture.QueryFrame()); //draw the image obtained from camera
+            });
+            viewer.ShowDialog(); //show the image viewer    
+        }
+
+        private Image<Bgr, Byte> checkCircles(Image<Bgr, Byte> img)
+        {
+            Image<Bgr, Byte> result = img;
+            Image<Gray, Byte> grayImage = img.Convert<Gray, Byte>().PyrDown().PyrUp();
+            
+            Gray cannyThreshold = new Gray(180);
+            Gray cannyThresholdLinking = new Gray(120);
+            Gray circleAccumulatorThreshold = new Gray(120);
+
+            CircleF[] circles = grayImage.HoughCircles(
+            cannyThreshold,
+            circleAccumulatorThreshold,
+            5.0, //Resolution of the accumulator used to detect centers of the circles
+            10.0, //min distance 
+            5, //min radius
+            50 //max radius
+            )[0]; //Get the circles from the first channel
+
+            foreach (CircleF circle in circles)
+                result.Draw(circle, new Bgr(Color.Red), 2);
+
+            return result;
         }
     }
 }
