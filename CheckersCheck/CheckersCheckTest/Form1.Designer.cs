16594a7a80bd345f42ddc071004e4965127c0b79
kebabkiller <ad.nikodem@gmail.com>
2014-05-20 16:39:16 +0200

    komi wprowadzajacy zmiany w kodzie programu

--- CheckersCheck/CheckersCheckTest/Form1.Designer.cs
+++ CheckersCheck/CheckersCheckTest/Form1.Designer.cs
@@ -29,33 +29,161 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(228, 111);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 65);
+            this.button1.Size = new System.Drawing.Size(76, 65);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(426, 12);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(340, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 225);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(518, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(536, 225);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(518, 500);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(310, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 65);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(390, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 65);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(426, 60);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Minimum = -255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(340, 40);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(470, 111);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 65);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(542, 331);
+            this.ClientSize = new System.Drawing.Size(1293, 908);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- CheckersCheck/CheckersCheckTest/Form1.cs
+++ CheckersCheck/CheckersCheckTest/Form1.cs
@@ -15,49 +15,367 @@ namespace CheckersCheckTest
 {
     public partial class Form1 : Form
     {
+        //bool binary = false;
+        private int binaryTreshold;
+        private int saturation;
+        private int mode;
+        private bool darkRigth;
+        private bool dotsRigth;
+        private bool contrast;
+        private int gaussianValue;
+        private bool gaussian;
+
+        List<MCvBox2D> boxList;
+        Capture capture;
+
         public Form1()
         {
+            contrast = false;
+            gaussianValue = 3;
+            gaussian = false;
+            boxList = new List<MCvBox2D>();//create a camera captue
+            capture = new Capture();
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            runCamera();
+            Application.Idle -= runCamera;
+
+
+
+            mode = 0;
+            Application.Idle +=  runCamera;
+
         }
 
-        private void runCamera()
+        private void runCamera(object sender, EventArgs e)
         {
-            ImageViewer viewer = new ImageViewer(); //create an image viewer
-            Capture capture = new Capture(); //create a camera captue
-            Application.Idle += new EventHandler(delegate(object sender, EventArgs e)
-            {  //run this until application closed (close button click on image viewer)
-                viewer.Image = checkCircles(capture.QueryFrame()); //draw the image obtained from camera
-            });
-            viewer.ShowDialog(); //show the image viewer    
+              CheckState state = checkBox1.CheckState;
+              if (state == CheckState.Checked)
+                contrast = true;
+              else
+                contrast = false;
+
+
+              state = checkBox2.CheckState;
+              if (state == CheckState.Checked)
+                  gaussian = true;
+              else
+                  gaussian = false;
+
+
+            switch (mode)
+            {
+                 case 0:
+                     mode0(capture);
+                     break;
+
+                 case 1:
+                     mode1(capture);
+                     break;
+
+                 case 2:
+                     mode2(capture);
+                     break;
+                 case 3:
+                     mode3(capture);
+                     break;
+                     
+           }
+
         }
 
-        private Image<Bgr, Byte> checkCircles(Image<Bgr, Byte> img)
+        private void mode0(Capture capture)
         {
-            Image<Bgr, Byte> result = img;
-            Image<Gray, Byte> grayImage = img.Convert<Gray, Byte>().PyrDown().PyrUp();
-            
-            Gray cannyThreshold = new Gray(180);
-            Gray cannyThresholdLinking = new Gray(120);
-            Gray circleAccumulatorThreshold = new Gray(120);
+            pictureBox1.Image = detectBoxes(capture.QueryFrame()).ToBitmap();
+        }
 
-            CircleF[] circles = grayImage.HoughCircles(
-            cannyThreshold,
-            circleAccumulatorThreshold,
-            5.0, //Resolution of the accumulator used to detect centers of the circles
-            10.0, //min distance 
-            5, //min radius
-            50 //max radius
-            )[0]; //Get the circles from the first channel
+        private void mode1(Capture capture)
+        {
+            pictureBox1.Image = boardCheck(capture.QueryFrame()).ToBitmap();
+        }
 
-            foreach (CircleF circle in circles)
-                result.Draw(circle, new Bgr(Color.Red), 2);
+        private void mode2(Capture capture)
+        {
+            pictureBox1.Image = capture.QueryFrame().ToBitmap();
+
+            Image<Bgr, Byte> previous = capture.QueryFrame();
+            System.Threading.Thread.Sleep(500);
+            Image<Bgr, Byte> last = capture.QueryFrame();
+
+            bool isMove = detectMovement(previous, last);
+
+            if (isMove == true) 
+                MessageBox.Show("There is a movement around here!");
+            else { }
+        }
+
+        private void mode3(Capture capture)
+        {
+            pictureBox1.Image = piecesCheck(capture.QueryFrame()).ToBitmap();
+        }
+
+        private Image<Hls, Byte> piecesCheck(Image<Bgr, Byte> img)
+        {
+            int channel = 1;
+            int black = 30;
+            int white = 99;
+
+            /*#region draw triangles and rectangles
+
+            foreach (MCvBox2D box in boxList)
+            {
+                img.Draw(box, new Bgr(Color.DarkOrange), 2);
+            }
+            #endregion*/
+
+            Image<Hls, Byte> result = new Image<Hls, byte>(img.Bitmap).PyrDown().PyrUp();
+
+            if (gaussian == true)
+                result = result.SmoothGaussian(gaussianValue);
+
+            if (contrast == true)
+                result._EqualizeHist();
+
+            result[2] += saturation;
+
+            //Image<Gray, Byte> temp = result[2];
+
+            int countBlack;
+            int countWhite;
+
+            for (int i = 0; i < 32; i++)
+            {
+                int x = (int)boxList[i].center.X;
+                int y = (int)boxList[i].center.Y;
+
+                countWhite = 0;
+                countBlack = 0;
+
+                byte asd = result.Data[y, x, 1];
+
+                if (asd > white)
+                    //countWhite++;
+                    result.Draw(new CircleF(boxList[i].center, 3), new Hls(120, 50, 100), 3);
+                if (asd < black)
+                    //countBlack++;
+                    result.Draw(new CircleF(boxList[i].center, 3), new Hls(220, 60, 100), 3);
+                
+                #region count Colors
+                /*
+                for (int j = 0; j < 22; j++)
+                {
+
+                    if ((asd = result.Data[y + 1 + j, x, 1]) > white)
+                        countWhite++;
+                    result.Draw(new CircleF(new PointF(y, x), 5), new Hls(220, 60, 100), 5);    
+                        
+                    if ((asd = result.Data[y + 1 + j, x, 1]) < black)
+                        countBlack++;
+
+                    if ((asd = result.Data[y - 1 - j, x, 1]) > white)
+                        countWhite++;
+                    if ((asd = result.Data[y - 1 - j, x, 1]) < black)
+                        countBlack++;
+
+                    if ((asd = result.Data[y, x + 1 + j, 1]) > white)
+                        countWhite++;
+                    if ((asd = result.Data[y, x + 1 + j, 1]) < black)
+                        countBlack++;
+
+                    if ((asd = result.Data[y, x - 1 - j, 1]) > white)
+                        countWhite++;
+                    if ((asd = result.Data[y, x - 1 - j, 1]) < black)
+                        countBlack++;
+
+                    if ((asd = result.Data[y - 1 - j, x - 1 - j, 1]) > white)
+                        countWhite++;
+                    if ((asd = result.Data[y - 1 - j, x - 1 - j, 1]) < black)
+                        countBlack++;
+
+                    if ((asd = result.Data[y + 1 + j, x - 1 - j, 1]) > white)
+                        countWhite++;
+                    if ((asd = result.Data[y + 1 + j, x - 1 - j, 1]) < black)
+                        countBlack++;
+
+                    if ((asd = result.Data[y - 1 - j, x + 1 + j, 1]) > white)
+                        countWhite++;
+                    if ((asd = result.Data[y - 1 - j, x + 1 + j, 1]) < black)
+                        countBlack++;
+
+                    if ((asd = result.Data[y + 1 + j, x + 1 + j, 1]) > white)
+                        countWhite++;
+                    if ((asd = result.Data[y + 1 + j, x + 1 + j, 1]) < black)
+                        countBlack++;
+
+                }
+                */
+                #endregion
+                /*
+                if (countWhite > 100)
+                    result.Draw(new CircleF(boxList[i].center, 3), new Hls(120, 50, 100), 3);
+
+                if (countBlack > 100)
+                    result.Draw(new CircleF(boxList[i].center, 3), new Hls(220, 60, 100), 3);*/
+            }
 
             return result;
+        }
+
+        private bool detectMovement(Image<Bgr, Byte> previous, Image<Bgr, Byte> last)
+        {
+            Image<Gray, Byte> grayPrevious = previous.Convert<Gray, Byte>().PyrDown().PyrUp().ThresholdBinary(new Gray(binaryTreshold), new Gray(255)); ;
+            Image<Gray, Byte> grayLast = last.Convert<Gray, Byte>().PyrDown().PyrUp().ThresholdBinary(new Gray(binaryTreshold), new Gray(255)); ;
+
+            /*if(gaussian == true)
+                result = result.SmoothGaussian(gaussianValue);
+
+            if(contrast == true)
+                result._EqualizeHist();*/
+
+            Image<Gray, byte> diffFrame = grayPrevious.AbsDiff(grayLast);
+            int moves = diffFrame.CountNonzero()[0];
+
+            if(moves>1) return true;
+            else return false;
+        }
+
+        private Image<Bgr, Byte> boardCheck(Image<Bgr, Byte> img)
+        {
+            #region draw triangles and rectangles
+            Image<Bgr, Byte> result = img;
+            foreach (MCvBox2D box in boxList)
+            {
+                result.Draw(box, new Bgr(Color.DarkOrange), 2);
+            }
+            #endregion
+
+            result.Draw(new CircleF(new PointF(boxList[0].center.X, boxList[0].center.Y), 3), new Bgr(Color.Blue), 3);
+
+            return result;
+        }
+
+        private Image<Bgr, Byte> detectBoxes(Image<Bgr, Byte> img)
+        {
+            Image<Bgr, Byte> result = img;
+
+            Image<Gray, Byte> grayImage = img.Convert<Gray, Byte>();
+            
+            
+            //grayImage._EqualizeHist();
+
+            
+            grayImage = grayImage.PyrDown().PyrUp();
+            if (gaussian == true)
+                //grayImage = grayImage.SmoothGaussian(gaussianValue);
+
+            //cannyFrame._Dilate(1);
+            grayImage._Dilate(3);
+
+            //Image<Gray, byte> cannyFrame = grayImage.Canny(120, 120);
+
+            //if(binary == true)
+            grayImage = grayImage.ThresholdBinary(new Gray(binaryTreshold), new Gray(255));
+
+            //Image<Gray, Byte> cannyEdges = grayImage.Canny(120, 120);
+            Bitmap grayBitmap = grayImage.ToBitmap();
+            pictureBox2.Image = grayImage.ToBitmap();
+
+            boxList = new List<MCvBox2D>();
+
+            using (MemStorage storage = new MemStorage()) //allocate storage for contour approximation
+                for (Contour<Point> contours = grayImage.FindContours(); contours != null; contours = contours.HNext)
+                {
+                    Contour<Point> currentContour = contours.ApproxPoly(contours.Perimeter * 0.05, storage);
+
+                    if (contours.Area > 250 && contours.Area < 20000) //only consider contours with area greater than 250
+                    {
+                        if (currentContour.Total == 4) //The contour has 4 vertices.
+                        {
+                            #region determine if all the angles in the contour are within the range of [80, 100] degree
+                            bool isRectangle = true;
+                            Point[] pts = currentContour.ToArray();
+                            LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                            for (int i = 0; i < edges.Length; i++)
+                            {
+                                double angle = Math.Abs(
+                                   edges[(i + 1) % edges.Length].GetExteriorAngleDegree(edges[i]));
+                                if (angle < 80 || angle > 100)
+                                {
+                                    isRectangle = false;
+                                    break;
+                                }
+                            }
+                            #endregion
+
+                            if (isRectangle) boxList.Add(currentContour.GetMinAreaRect());
+                        }
+                    }
+                }
+
+            #region draw triangles and rectangles
+            Image<Bgr, Byte> rectangleImage = result;
+            foreach (MCvBox2D box in boxList)
+            {
+                rectangleImage.Draw(box, new Bgr(Color.DarkOrange), 2);
+            }
+            #endregion
+
+            try
+            {
+                for (int i = 0; i < 32; i++)
+                {
+                    if (i == 0)
+                        rectangleImage.Draw(new CircleF(new PointF(boxList[i].center.X, boxList[i].center.Y), 3), new Bgr(Color.Blue), 3);
+                    else
+                        rectangleImage.Draw(new CircleF(new PointF(boxList[i].center.X, boxList[i].center.Y), 3), new Bgr(Color.Red), 3);
+                }
+            }
+            catch { }
+
+            return rectangleImage;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            binaryTreshold = trackBar1.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Idle -= runCamera;
+            mode = 1;
+
+            Application.Idle += runCamera;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Idle -= runCamera;
+            mode = 2;
+
+            Application.Idle += runCamera;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            saturation = trackBar2.Value;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Idle -= runCamera;
+            mode = 3;
+            pictureBox2.Image = null;
+
+            Application.Idle += runCamera;
         }
     }
 }
